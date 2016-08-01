5439ba8d7ee397f9520b2aeea51097d6381ecdae
Brian Grant and Joel Delight <brian+joel@>
2016-08-01 15:09:43 -0700

    create the basic draw function and start on user-interface.

--- js/game.js
+++ js/game.js
@@ -6,5 +6,18 @@ function Game() {
 Game.prototype.guess = function(guessSquare) {
   if(gameState.toString().charAt(guessSquare) === "0") {
     gameState += Math.pow(10, 12 - guessSquare);
+    var gameStateString = gameState.toString();
+    var revealedGuesses = gameStateString.replace(/[^1]/, "").length;
+    if (revealedGuesses === 2) {
+      var guess1 = gameStateString.indexOf("1");
+      var guess2 = gameStateString.indexOf("1", guess1 + 1);
+      if (shuffledGame.charAt(guess1) === shuffledGame.charAt(guess2)) {
+        gameState = gameState + Math.pow(10, guess1) + Math.Pow(10, guess2);
+      } else {
+        gameState = gameState - Math.pow(10, guess1) - Math.Pow(10, guess2);
+      }
+
+    }
+
   }
 };
